f58b6a4dd909fcc8641b5c1dbebd5ab5cfbb0747
katetriestocode <caterina.camerlengo@icloud.com>
2026-03-07 00:40:07 +0000

    fin

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
 from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.rotary_encoder import RotaryEncoderHandler
+from kmk.extensions.RGB import RGB
 from kmk.extensions.OLED import OLED
 
 keyboard = KMKKeyboard()
@@ -14,13 +16,6 @@ encoder_handler = RotaryEncoderHandler()
 #keybinds for shortcuts (to configure in puter)
 OPEN_DISCORD = KC.LCTL(KC.LALT(KC.LSFT(KC.D)))
 OPEN_OBS = KC.LCTL(KC.LALT(KC.LSFT(KC.C)))
-OPEN_APP = KC.LCTL(KC.LALT(KC.LSFT(KC.E)))
-
-#keyextensions
-keyboard.extensions.append(MediaKeys())
-keyboard.extensions.append(macros)
-keyboard.extensions.append(oled)
-keyboard.extensions.append(rgb)
 
 
 oled = OLED(
@@ -30,14 +25,24 @@ oled = OLED(
     i2c_scl= board.GP28,
 )
 
-# Encoder Rotation: CW = Vol Up, CCW = Vol Down
-encoder_handler.map = [
-    ((KC.VOLU, KC.VOLD),), 
-]
-
 rgb = RGB(pixel_pin=board.GP26, num_pixels=2)
 
-PINS = [board.GP1, board.GP2, board.GP0, board.GP6, board.GP7, board.GP29, board.GP4]
+
+#keyextensions
+keyboard.extensions.append(MediaKeys())
+keyboard.extensions.append(macros)
+keyboard.extensions.append(oled)
+keyboard.extensions.append(rgb)
+
+
+
+encoder = RotaryEncoderHandler(pin_a=board.GP3, pin_b=board.GP4)
+encoder.rotation_cw = KC.VOLU 
+encoder.rotation_ccw = KC.VOLD 
+keyboard.modules.append(encoder)
+
+
+PINS = [board.GP1, board.GP2, board.GP0, board.GP6, board.GP7, board.GP29]
 
 keyboard.matrix = KeysScanner(
     pins = PINS,
@@ -47,13 +52,12 @@ keyboard.matrix = KeysScanner(
 #keymap
 keyboard.keymap = [
     [
+    KC.AUDIO_MUTE
+    OPEN_DISCORD
+    OPEN_OBS
     KC.MEDIA_PLAY_PAUSE
     KC.MEDIA_NEXT_TRACK
     KC.MEDIA_PREV_TRACK
-    KC.AUDIO_MUTE
-    KC.OPEN_DISCORD
-    KC.OPEN_OBS
-    KC.OPEN_APP
     ]
 ]
 
